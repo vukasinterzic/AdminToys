4bff79e2f527f2cdb8f63a8e9f50aabb6f538a11
Vukasin Terzic <37403397+vukasinterzic@users.noreply.github.com>
2018-05-11 13:25:03 -0700

    1.0

--- OS/Get-DiskSpace.ps1
+++ OS/Get-DiskSpace.ps1
@@ -9,23 +9,19 @@ This function help you find and export disk space information from local or remo
 
 .PARAMETER Computer
 
-Specifies the name computer that needs to be scanned.
-
-.PARAMETER ExportPath
-
-Specify export file.
+Specifies the name computer or computers.
 
 .PARAMETER SizeIn
 
-Specify in which unit will disk size be displayed. It can be in MB, GB or TB.
+Specify in which unit will disk size be displayed. It can be in MB, GB or TB. Default size is in GB
 
-.PARAMETER WriteToLog
+.PARAMETER ExportPath
 
-If this parameter is used, log will be writtend in file. Specify path.
+Specify export file path, including file name.
 
 .INPUTS
 
-None; Computer(s) name(s); SizeIn;
+None; Computer(s) name(s); SizeIn; ExportPath
 
 .OUTPUTS
 
@@ -37,7 +33,7 @@ C:\PS> Get-DiskSpace
 
 .EXAMPLE
 
-C:\PS> Get-DiskSpace -Computer COMPUTERNAME
+C:\PS> Get-DiskSpace -Computers COMPUTERNAME1 -SizeIn TB -ExportPath C:\export.csv
 
 .EXAMPLE
 
@@ -50,121 +46,93 @@ http://www.vukasinterzic.com
 #>
 
 
-#create function, that can be used on any PC or multiple PCs
-#no computer name paramter = local pc, multiple names = multiple PCs check
-#add results to object and then display sorted
-#add parameter for export to file
-#add parameter for size conversion (MB,GB,TB)
-#add parameter pro Logovani (nova funkce?)
-
-# OSETRIT ERROR A kdyz nepujde gwmi tak zkusit Get-CimInstance
-# v pripade chyby vypsat chybu a zapsat radek do logu/exportu
-
-
 function Get-DiskSpace {
     [CmdletBinding()]
 
     param 
     (
-        [Parameter(ValueFromPipeline = $true,
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true)]
-        [string]$Computers = ".",
+        [array]$Computers = "localhost",
         
         [Parameter(ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true)]
-        [string]$ExportPath = "",
-        
-        [Parameter(ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("MB", "GB", "TB")]
         [string]$SizeIn = "",
-
+        
         [Parameter(ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true)]
-        [string]$WriteToLog = "",
-
-        [Parameter(ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true)]
-        [switch]$Full
+        [string]$ExportPath = ""
+    
     )
 
     begin {
         Write-Verbose "Function begin. Finding disk space."
     }
 
+    process {
+
+        
+        # hashtable for conversion of Disk Types
+        $typeHash = @{
+
+            2 = "Removable disk"
+            3 = "Fixed local disk"
+            4 = "Network disk"
+            5 = "Compact disk"
+
+        }
+
+        $sizeDivide = "1" + $SizeIn
+
+        # Computer loop
+        foreach ($Computer in $Computers) {
+
+            Try {
+                $allDisks = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $Computer -ErrorAction Stop |
+                    Select-Object @{n = "ComputerName"; e = {$_.PSCOmputerName}},
+                @{n = "Type"; e = {$typeHash.item([int]$_.DriveType)}},
+                ProviderName, DeviceID, VolumeName,
+                @{n = "Size($SizeIn)"; e = {"{0:N2}" -f ($_.Size / $sizeDivide)}},
+                @{n = "FreeSpace($SizeIn)"; e = {"{0:N2}" -f ($_.FreeSpace / $sizeDivide)}},
+                @{n = "UsedSpace($SizeIn)"; e = {"{0:N2}" -f (($_.Size - $_.FreeSpace) / $sizeDivide)}}
+
+                #show results:    
+                Write-Host -ForegroundColor Yellow "Disks information for computer " -NoNewline
+                Write-Host -ForegroundColor Green  "$Computer" -NoNewline
+                Write-Host -ForegroundColor Yellow ":"
+    
+                $allDisks | Format-Table -AutoSize
+
+                if ($ExportPath) {
+                    $allDisks | Export-Csv -Path $ExportPath -Delimiter ";" -NoTypeInformation -Append
+                }
+    
+            }
+            
+            Catch {
+
+                Write-Host -ForegroundColor Yellow "There was an error while connecting to " -NoNewline
+                Write-Host -ForegroundColor Red  "$Computer" -NoNewline
+                Write-Host -ForegroundColor Yellow ". Error message is bellow:"
+                Write-Host ""
+                Write-Host -ForegroundColor Red $($_.Exception.message)
+                Write-Host ""
+            } 
+
+        } #end of Computers loop
 
 
-}
+    } #end of Process
 
+
+} #end of Get-DiskSpace
 
 <#
-if ($Full) {
-    $selectProperties = @("*")
-}
-else {
-    $selectProperties = @("")
-}
+To Do:
+
+Add error detection and verbose
+Add SUM for each individual server
+
 #>
-
-if ($Computers) {
-
-    foreach ($Computer in $Computers) {
-
-        $allDisks = Get-CimInstance -ClassName Win32_LogicalDisk -ComputerName $Computer | Select-Object DeviceID, DriveType, VolumeName, Size, FreeSpace, @{n = 'UsedSpace'; e = {$_.Size - $_.FreeSpace}}
-        
-    }
-
-}
-
-
-
-<#plan: check error
-if no error continue
-create object and add:
-properties
-convert sizes depending on switch
-convert disk tyep to text
-calculate sum and add to same object
-#>
-
-
-
-
-
-
-
-write `n`r 
-$MyString = Get-WmiObject win32_logicaldisk | select DeviceID, VolumeName, @{n = "Size(GB)"; e = {" {0:N2}" -f ($_.Size / 1GB)}}, @{n = "FreeSpace(GB)"; e = {" {0:N2}" -f ($_.FreeSpace / 1GB)}}, @{n = "UsedSpace(GB)"; e = {" {0:N2}" -f (($_.Size - $_.FreeSpace) / 1GB)}} | ft
-($MyString | Out-String).Trim()
-
-$WorkVar = Get-WmiObject win32_logicaldisk | select DeviceID, VolumeName, Size, FreeSpace, @{n = "UsedSpace"; e = {$_.Size - $_.FreeSpace}}
-
-$totalsize = 0
-$totalfree = 0
-$totalused = 0
-
-foreach ($disk in $WorkVar) {
-    $totalsize += $disk.Size
-    $totalfree += $disk.FreeSpace
-    $totalused += $disk.UsedSpace
-}
-
-$totalsizeTB = "{0:N2}" -f ($totalsize / 1tb)
-$totalfreeTB = "{0:N2}" -f ($totalfree / 1tb)
-$totalusedTB = "{0:N2}" -f ($totalused / 1tb)
-
-$totalCount = "Total count:        $totalsizeTB TB  $totalfreeTB TB       $totalusedTB TB"
-Write-Host $totalCount -ForegroundColor Green
-write `n`r 
-
-
-
-$typeHash = @{
-
-    2 = "Removable disk"
- 
-    3 = "Fixed local disk"
- 
-    4 = "Network disk"
- 
-    5 = "Compact disk"
-}
