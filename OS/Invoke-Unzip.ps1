e9c2f92d3a8650d9b63e7052afbb8ff9724c781a
Vukasin Terzic <vukasin.terzic@outlook.com>
2019-12-06 14:10:01 +0100

    new function

---
+++ OS/Invoke-Unzip.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+This function performs zip file extraction. 
+
+.DESCRIPTION
+Zip file extraction by attempting 3 methods, in case prerequisites are not met.
+
+.PARAMETER ZipFile
+Specifies the name or path of the zipped file that needs to be extracted.
+
+.PARAMETER ExportPath
+Specify the path for the exported files.
+
+.EXAMPLE
+C:\PS> Invoke-Unzip -ZipFile c:\file.zip -ExportPath c:\exported
+
+.LINK
+https://github.com/vukasinterzic/AdminToolBox
+
+#>
+
+function Invoke-Unzip {
+    [cmdletbinding()]
+    param(
+        [string]$ZipFile,
+        [string]$ExportPath
+    )
+
+    if (Get-Command expand-archive) {
+        $ErrorActionPreference = 'SilentlyContinue'
+        Expand-Archive -Path $ZipFile -DestinationPath $ExportPath
+        $ErrorActionPreference = 'Continue'
+    }
+
+    else {
+        try {
+            #Allows for unzipping folders in older versions of powershell if .net 4.5 or newer exists
+            Add-Type -AssemblyName System.IO.Compression.FileSystem
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFile, $ExportPath)
+        }
+    
+        catch {
+            #If .net 4.5 or newer not present, com classes are used. This process is slower.
+            [void] (New-Item -Path $ExportPath -ItemType Directory -Force)
+            $Shell = New-Object -com Shell.Application
+            $Shell.Namespace($outpath).copyhere($Shell.NameSpace($ZipFile).Items(), 4)
+        }
+    }
+}
