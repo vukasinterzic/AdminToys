06b279def4687b8ac02396f721fba937f898d6bc
Vukasin Terzic <vukasin.terzic@outlook.com>
2019-12-11 12:53:16 +0100

    created font preview list and function.

--- Misc/Convert-TextToASCIIArt.ps1
+++ Misc/Convert-TextToASCIIArt.ps1
@@ -9,7 +9,7 @@ Convert text. Output is also copied to sclipboard.
 Add text. Parameter is mandatory.
 
 .PARAMETER Font
-Select different font of your text. You can find font list and preview here: 
+Select different font of your text. You can find font preview in Font-Preview.html page
 
 .INPUTS
 Text;
@@ -41,14 +41,26 @@ function Convert-TextToASCIIArt {
     )
 
     begin {
-        Write-Verbose "Function Convert-TextToASCIIArt is called"
+        Write-Verbose -Message "Function Convert-TextToASCIIArt is called"
     }
 
     process {
         
+        Write-Verbose -Message "Replacing spaces with + ..."
         $Text = $Text.Replace(" ","+")
-        $URL = "http://artii.herokuapp.com/make?text="+$Text
-        $ASCIIArtText = (Invoke-WebREquest -URI $URL).Content
+        if ($Font) {
+            Write-Verbose -Message "Font selected. Checking if font name is valid..."
+            Write-Verbose -Message "Getting the list of fonts..."
+            
+
+            $URL = "http://artii.herokuapp.com/make?text="+$Text+"&font="+$Font
+        } else {
+            $URL = "http://artii.herokuapp.com/make?text="+$Text
+        }
+
+
+        
+        $ASCIIArtText = (Invoke-WebRequest -URI $URL).Content
 
         $ASCIIArtText | clip
 
@@ -65,5 +77,50 @@ function Convert-TextToASCIIArt {
     }
 
 }
+
+<#
+#>
+
+#Get the font list:
+
+$FontsPageContent = (Invoke-WebRequest "http://artii.herokuapp.com/fonts_list").Content
+$fonts = @()
+$fonts = $FontsPageContent.Split([Environment]::NewLine)
+
+$Body = @()
+$Body +="
+<!DOCTYPE html>
+<html>
+
+<body>
+<h2>Font preview page</h2>
+<table>
+<tr>
+<th>FontName</th>
+<th>FontPreview</th>
+<tr>
+"
+$FontPreview = @()
+foreach ($Font in $Fonts) {
+
+    
+    $URL = "http://artii.herokuapp.com/make?text=Text&font="+$Font
+    $FontPreview = (Invoke-WebRequest -Uri $URL).Content
+
+    $Body += "
+    <tr>
+    <td>$Font</td>
+    <td><pre><code>$FontPreview</code></pre></td>
+    <tr>
+    "
+
+}
+
+$Body += "</table></body></html>"
+
+$Body | Out-File "Font-Preview.html"
+
+
+
 
 #TODO Add font selection option
