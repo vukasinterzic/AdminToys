7d1849045953705bd7c318dad2bf0f153d7efde2
vukasinterzic <vukasin.terzic@outlook.com>
2022-01-05 21:50:15 -0800

    renamed file

---
+++ OS/Test-Elevation.ps1
@@ -0,0 +1,8 @@
+function Test-Elevation  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+#TODO Add description
+#TODO Add Set option, to re-open window with Admin priviledges
